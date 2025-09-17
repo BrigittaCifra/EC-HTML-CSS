dd8273852a1744d2c883eccc8aac8e7e22a920dd
Brigitta Cifra <brigittacifra@hotmail.com>
2025-09-17 09:18:30 +0200

    Gjorde uppdateringar till artikel sidan, la till en inloggning modal, la till en till breakpoint för laptops

--- V3/javascript.js
+++ V3/javascript.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const mobileNavBtn = document.querySelector('.mobile-nav-btn');
     const mobileNav = document.querySelector('.mobile-nav');
 
+    const open = document.getElementById('modal-btn-open');
+    const modal_wrapper = document.getElementById('modal-wrapper');
+    const close = document.getElementById('modal-btn-close');
+
     console.log('Menu button:', menuButton);
     console.log('Nav links:', mobileNavLinks);
     console.log('Nav btn:', mobileNavBtn);
@@ -17,4 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
             mobileNav.classList.toggle('menu-open');
         });
     }
+
+    open.addEventListener('click', () => {
+        modal_wrapper.classList.add('show');
+    });
+
+    close.addEventListener('click', () => {
+        modal_wrapper.classList.remove('show');
+    });
 });
