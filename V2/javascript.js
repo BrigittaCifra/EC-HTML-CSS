405d6bbc69c9fe970a245a897b83e42393e30b75
Brigitta Cifra <brigittacifra@hotmail.com>
2025-09-30 11:04:16 +0200

    La till en gitignore fil och unstagade 3 mappar

--- V2/javascript.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("top-nav-item");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "flex";
-    }
-}
-
